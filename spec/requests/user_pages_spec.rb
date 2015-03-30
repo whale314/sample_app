deb6e970689b5f5ffbd78ad02d3409a8768539c6
whale52hz <onelinewithasmallletter@gmail.com>
2015-03-30 14:30:27 +0300

    model with passwords

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,12 +3,11 @@ include ApplicationHelper
 
 describe "User pages" do
 
-  subject { response.body }
-
   describe "signup page" do
     before { get signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it 'have sign up content' do
+      expect( response.body[/Sign up/] ).to eq( 'Sign up' )
+    end
   end
 end
